5e98b127bb21cc7f901377dbf31e8a9086a37268
Patrick Fien <pmfien99@icloud.com>
2023-07-31 23:48:12 -0400

    Initial setup

---
+++ dist/bundle204fefef9cf455a6d069.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find package '@babel/preset-env' imported from C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\babel-virtual-resolve-base.js\n    at new NodeError (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\vendor\\import-meta-resolve.js:203:5)\n    at packageResolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\vendor\\import-meta-resolve.js:873:9)\n    at moduleResolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\vendor\\import-meta-resolve.js:902:20)\n    at defaultResolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\vendor\\import-meta-resolve.js:985:15)\n    at resolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\vendor\\import-meta-resolve.js:999:12)\n    at resolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\config\\files\\import-meta-resolve.js:13:10)\n    at tryImportMetaResolve (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:137:45)\n    at resolveStandardizedNameForImport (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:159:19)\n    at resolveStandardizedName (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:168:12)\n    at loadPreset (C:\\Users\\pmfien\\Desktop\\Professional\\Code Projects\\fit-this\\node_modules\\@babel\\core\\lib\\config\\files\\plugins.js:56:20)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle204fefef9cf455a6d069.js.map
